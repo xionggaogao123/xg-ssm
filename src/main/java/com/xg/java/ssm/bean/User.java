c03c5c8d6dfcf356569c376ba4a3d45da5f09a86
颜鑫宇 <yanxinyu@terminus.io>
2017-05-11 15:37:35 +0800

    Initial commit

---
+++ src/main/java/com/xg/java/ssm/bean/User.java
@@ -0,0 +1,27 @@
+package com.xg.java.ssm.bean;
+
+/**
+ * Created by xionggao on 2017/5/11.
+ */
+public class User {
+    private Long id;
+    private String name;
+
+    public User(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
